b6beabb08a41c991f127888c81b9c953773638b7
FabianoDantas <fabianorsdantas@gmail.com>
2024-12-02 08:07:08 -0300

    Estrutura app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-export default function App(){
-    return (
-        <div>
-            Digite aqui sua aplicação
-        </div>
-    )
+import { useState } from "react";
+import Home from "./components/Home";
+import Recall from "./components/Recall";
+import styled from "styled-components";
+
+export default function App() {
+  const [isHome, setIsHome] = useState(true);
+  return <Page>{isHome ? <Home setIsHome={setIsHome} /> : <Recall setIsHome={setIsHome} />}</Page>;
 }
 
+const Page = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background: #fb6b6b;
+`;
